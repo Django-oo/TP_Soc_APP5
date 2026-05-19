43dd8e7ff0955e60dc024c7177b90921fac2fd71
unknown <secours@a-201579.upsay.universite-paris-saclay.fr>
2026-05-19 09:32:43 +0200

    debugging

--- nios_system/synthesis/submodules/PWM_avalon_interface.vhd
+++ nios_system/synthesis/submodules/PWM_avalon_interface.vhd
@@ -6,34 +6,33 @@ entity PWM_avalon_interface is
         clock        : in  std_logic;
         reset_n      : in  std_logic;
 
-        address      : in  std_logic_vector(1 downto 0);
-        read         : in  std_logic;
-        write        : in  std_logic;
+        address      : in  std_logic_vector(0 downto 0);
         chipselect   : in  std_logic;
-        writedata    : in  std_logic_vector(15 downto 0);
-        byteenable   : in  std_logic_vector(1 downto 0);
-        readdata     : out std_logic_vector(15 downto 0);
+        write        : in  std_logic;
+        read         : in  std_logic;
+        byteenable   : in  std_logic_vector(3 downto 0);
+        writedata    : in  std_logic_vector(31 downto 0);
+        readdata     : out std_logic_vector(31 downto 0);
 
-        dc_motor_p_R : out std_logic;
-        dc_motor_n_R : out std_logic;
-        dc_motor_p_L : out std_logic;
-        dc_motor_n_L : out std_logic
+        motor_out    : out std_logic_vector(3 downto 0)
     );
 end entity PWM_avalon_interface;
 
 architecture rtl of PWM_avalon_interface is
-    signal command_right : std_logic_vector(13 downto 0);
-    signal command_left  : std_logic_vector(13 downto 0);
+    signal command_right : std_logic_vector(13 downto 0) := (others => '0');
+    signal command_left  : std_logic_vector(13 downto 0) := (others => '0');
+
+    constant ZERO18 : std_logic_vector(17 downto 0) := (others => '0');
 begin
-    process(clock)
+    process(clock, reset_n)
     begin
-        if rising_edge(clock) then
-            if reset_n = '0' then
-                command_right <= (others => '0');
-                command_left  <= (others => '0');
-            elsif chipselect = '1' and write = '1' then
+        if reset_n = '0' then
+            command_right <= (others => '0');
+            command_left  <= (others => '0');
+        elsif rising_edge(clock) then
+            if chipselect = '1' and write = '1' then
                 case address is
-                    when "00" =>
+                    when "0" =>
                         if byteenable(0) = '1' then
                             command_right(7 downto 0) <= writedata(7 downto 0);
                         end if;
@@ -41,7 +40,7 @@ begin
                             command_right(13 downto 8) <= writedata(13 downto 8);
                         end if;
 
-                    when "01" =>
+                    when "1" =>
                         if byteenable(0) = '1' then
                             command_left(7 downto 0) <= writedata(7 downto 0);
                         end if;
@@ -56,39 +55,15 @@ begin
         end if;
     end process;
 
-    process(address, read, chipselect, command_right, command_left)
-    begin
-        readdata <= (others => '0');
-
-        if chipselect = '1' and read = '1' then
-            case address is
-                when "00" =>
-                    readdata(13 downto 0) <= command_right;
-
-                when "01" =>
-                    readdata(13 downto 0) <= command_left;
-
-                when "10" =>
-                    readdata(0) <= command_right(13);
-                    readdata(1) <= command_left(13);
-                    readdata(2) <= command_right(12);
-                    readdata(3) <= command_left(12);
-
-                when others =>
-                    readdata <= (others => '0');
-            end case;
-        end if;
-    end process;
+    readdata <= ZERO18 & command_right when address = "0" else
+                ZERO18 & command_left;
 
     pwm_core : entity work.PWM_generation
         port map (
             clk           => clock,
             reset_n       => reset_n,
-            s_writedataR  => command_right,
-            s_writedataL  => command_left,
-            dc_motor_p_R  => dc_motor_p_R,
-            dc_motor_n_R  => dc_motor_n_R,
-            dc_motor_p_L  => dc_motor_p_L,
-            dc_motor_n_L  => dc_motor_n_L
+            command_right => command_right,
+            command_left  => command_left,
+            motor_out     => motor_out
         );
 end architecture rtl;
